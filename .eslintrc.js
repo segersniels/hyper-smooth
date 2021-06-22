76601704e2bb043f8324a59617c6ad675f086079
segersniels <segers.n@hotmail.com>
2021-06-22 19:07:10 +0200

    :wrench: Remove react linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,11 @@
 module.exports = {
   root: true,
-  env: {
-    node: true,
-  },
   parser: "@typescript-eslint/parser",
   plugins: ["@typescript-eslint", "simple-import-sort"],
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
-    "plugin:react/recommended",
-    "plugin:react-hooks/recommended",
   ],
-  settings: {
-    react: {
-      version: "detect",
-    },
-  },
   rules: {
     "@typescript-eslint/explicit-module-boundary-types": "off",
     "simple-import-sort/imports": "error",
